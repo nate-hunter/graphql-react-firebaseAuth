38561a01b79352c6ad7be833982324f6ee93e2ef
njh <nate.indemand@gmail.com>
2021-06-09 23:10:11 -0400

    basic apollo server

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require('express');
+const { ApolloServer } = require('apollo-server');
 
 require('dotenv').config();
 
-const app = express();
+const typeDefs = `
+    type Query {
+        totalPosts: Int!
+    }
 
-app.get('/panda', (req, res) => {
-    res.send('<h1>panda</h1>')
-})
+
+`;
+
+const resolvers = {
+    Query: {
+        totalPosts: () => 42
+    }
+};
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 const PORT = process.env.PORT || 8005;
 
-app.listen(PORT, () => {
+apolloServer.listen(PORT, () => {
     console.log(`--> Server ready at http://localhost:${PORT}`)
 })
+
